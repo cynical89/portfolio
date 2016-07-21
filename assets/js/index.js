3e8673198c14f54f65035320488cfa23c0631784
Anthony Michelizzi <cynical89@gmail.com>
2016-07-21 15:53:32 -0700

    added jquery-ui features
    navbar buttons fade in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,28 @@
+//clipboard.js for copying to clipboard
 new Clipboard("#email");
 
+//hide all the social buttons
+$("#github").hide();
+$("#facebook").hide();
+$("#gmail").hide();
+
+//on document ready
 $(document).ready(function () {
-	$("#email").click(function() {
-			$("#email").tooltip('toggle')
+	//toggle toolip on gmail button pressed
+	$("#gmail").click(function() {
+			$("#gmail").tooltip('toggle')
 			setTimeout(function(){
-				$("#email").tooltip('destroy')
+				$("#gmail").tooltip('destroy')
 			}, 800);
 	});
+//show all social buttons slowly
+setTimeout(function(){
+	$("#gmail").show("fade", "slow", 1000);
+}, 300);
+setTimeout(function(){
+	$("#facebook").show("fade", "slow", 1000);
+}, 1000);
+setTimeout(function(){
+	$("#github").show("fade", "slow", 1000);
+}, 1700);
 });
